01229ec3e0ebd2644540d112558e293ec48ae700
sekibura <51335422+sekibura@users.noreply.github.com>
2021-03-08 21:47:00 +0300

    start

---
+++ SomeExamples/Assets/Platformer/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicObject
+{
+    public float jumpTakeOffSpeed = 7;
+    public float maxSpeed = 7;
+
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if(Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * .5f;
+        }
+
+        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < -0.01f));
+        if (flipSprite)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+        }
+
+        animator.SetBool("grounded", grounded);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        animator.SetFloat("velocityY", velocity.y);
+        
+
+        targetVelocity = move * maxSpeed;
+    }
+}
