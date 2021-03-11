2d32b4da3c59ef8e4f53c3e080239c47da96f2ea
sekibura <51335422+sekibura@users.noreply.github.com>
2021-03-11 23:47:35 +0300

    add attack and health system

---
+++ SomeExamples/Assets/Platformer/Scripts/HealthSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHp;
+    private int currentHp;
+
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+
+   public void Damage(int damageValue)
+    {
+        Debug.Log(this.name+" - i was damaged!");
+        currentHp -= damageValue;
+        //TODO
+        //play hurt anim!
+        if (currentHp <= 0)
+            toDie();
+
+    }
+
+    private void toDie()
+    {
+        Debug.Log("its time to die...");
+        // TODO
+        //play die anim
+    }
+
+    public void AddHp(int value)
+    {
+        currentHp += value;
+        if (currentHp > maxHp)
+            currentHp = maxHp;
+    }
+}
---
+++ SomeExamples/Assets/Platformer/Scripts/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator animator;
+    public Transform attackPoint;
+    private float attackPointDefaultPositionX;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    private SpriteRenderer spriteRenderer;
+    public int damagePoints;
+
+  
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
+        attackPointDefaultPositionX = attackPoint.localPosition.x;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Debug.Log(spriteRenderer.flipX);
+        attackPoint.localPosition= new Vector3(spriteRenderer.flipX ? -attackPointDefaultPositionX: attackPointDefaultPositionX, attackPoint.localPosition.y, attackPoint.localPosition.z);
+        animator.SetTrigger("Attack");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log(enemy.name + " attacked");
+            enemy.GetComponent<HealthSystem>().Damage(damagePoints);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
