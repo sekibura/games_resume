27315158b0825af1b5cd51470e2801465aa2cbcf
Onizuka <51335422+sekibura@users.noreply.github.com>
2021-04-19 20:54:34 +0300

    doors code and change lvl

--- SomeExamples/Assets/Platformer/Scripts/BaseLevelElements/FinishDoor.cs
+++ SomeExamples/Assets/Platformer/Scripts/BaseLevelElements/FinishDoor.cs
@@ -1,28 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool IsFinishDoor = false;
+    public GameObject ExitDoor;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (IsThisPlayer(collision))
         {
-            NextLevel();
+            NextLevel(collision.gameObject);
         }
     }
 
-    private void NextLevel()
+    private void NextLevel(GameObject player)
     {
         //TODO
         //to next level!
         Debug.Log("Switch lvl");
+
+        //if last door on level
+        if (IsFinishDoor)
+            SwitchScene();
+        else
+            MoveToExitDoor(player);
+    }
+
+    private void SwitchScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void MoveToExitDoor(GameObject player)
+    {
+        if(ExitDoor!=null)
+            player.transform.position = ExitDoor.transform.position;
     }
 
     private bool IsThisPlayer(Collider2D collider)
