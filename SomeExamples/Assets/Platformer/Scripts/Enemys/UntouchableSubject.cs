50e8ce2eb844fc2b33b0f393f481e9266970cf3e
Onizuka <51335422+sekibura@users.noreply.github.com>
2021-06-03 22:10:52 +0300

    some updates
    Add hell, traps, mushrooms.

---
+++ SomeExamples/Assets/Platformer/Scripts/Enemys/UntouchableSubject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UntouchableSubject : MonoBehaviour
+{
+    [SerializeField]
+    private int _damageValue = 2;
+
+ 
+    
+ 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (IsThisPlayer(collision))
+        //{
+        //    //collision.GetComponent<PlayerController>().Attacked(gameObject); // impulse player
+        //    collision.gameObject.GetComponent<HealthSystem>().ApplyDamage(_damageValue,gameObject.transform.position); // 
+        //}
+        collision.gameObject.GetComponent<Attackable>()?.ApplyDamage(_damageValue, gameObject.transform.position); // 
+    }
+
+    //private bool IsThisPlayer(Collider2D collider)
+    //{
+    //    return collider.gameObject.layer == LayerMask.NameToLayer("Player");
+    //}
+
+}
